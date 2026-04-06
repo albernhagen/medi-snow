b57e02f826af0caa58ba9229565b00e38ca2af50
albernhagen <andybernhagen@gmail.com>
2026-04-06 10:07:19 -0600

    Rename to 'medi'

--- backend/medi-meterology/cmd/api/docs.go
+++
@@ -1,9 +0,0 @@
-package main
-
-// @title Medi-Meteorology API
-// @version 1.0.0
-// @description Weather and avalanche forecasting API for ski areas
-// @contact.name API Support
-// @contact.email support@example.com
-// @host localhost:8080
-// @BasePath /
